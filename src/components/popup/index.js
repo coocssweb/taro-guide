3f7f8ed9bb61276c6f9f64f13e0d4f69fcc1c022
coocssweb <1974740999@qq.com>
2019-05-31 19:12:53 +0800

    add animation

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -7,7 +7,7 @@ export default class Popup extends Component {
     constructor () {
         // this.haveOpened = false;
         this.state = {
-            
+            hide: false
         };
     }
 
@@ -15,18 +15,35 @@ export default class Popup extends Component {
         this.props.onClose();
     }
 
+    handleContentAnimationEnd = () => {
+
+    }
+
     render () {
         const { visible } = this.props;
+        const { hide } = this.state;
         // ##todo， 
         // haveOpened false =>return null
         // 发现有问题，后续可以优化性能
         // this.haveOpened = this.haveOpened || visible;
         console.log(visible);
+        const maskCls = `popup-mask `;
+        const contentCls = 'popup-content';
+        if (visible) {
+            maskCls += 'popup-mask--open ';
+            contentCls += 'popup-content--open ';
+        }
+        if (hide) {
+            maskCls += 'popup-mask--hide ';
+            contentCls += 'popup-content--hide ';
+        }
+
         return (
             <View className={`popup ${visible ? 'popup--open' : ''}`}>
                 <View onClick={this.handleMaskClick}
-                  className={`popup-mask ${visible ? 'popup-mask--open' : ''}`}></View>
-                <View className={`popup-content ${visible ? 'popup-content--open' : ''}`}>
+                  className={maskCls}></View>
+                <View className={`popup-content ${visible ? 'popup-content--open' : ''}`} 
+                  onAnimationEnd={this.handleContentAnimationEnd}>
                     { this.props.children }
                 </View>
             </View>
